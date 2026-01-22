ca66da99b3c89a67365aac2da499d4d34780df68
love-BL <3587133901@qq.com>
2026-01-22 19:30:20 +0800

    Add files via upload

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -7,27 +7,27 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "残颜&blog",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "一半烟火，一半笔墨。",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://blog.lovecanyan.top",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"我喜欢研究HTML编程，平时爱打游戏和看动漫，常玩暗区、王者和PUBG。",
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
-		"Fuwari",
-		"Astro",
-		"ACGN",
-		"博客",
+		"编程",
+		"html编程",
+		"网站服务器",
+		"残颜博客",
+		"残颜博客",
 		"技术博客",
-		"静态博客",
+		"研究交流",
 	],
 
 	// 主题色
@@ -79,7 +79,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2025-01-01",
+	siteStartDate: "2025-01-22",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
@@ -106,7 +106,7 @@ export const siteConfig: SiteConfig = {
 	// bangumi配置
 	bangumi: {
 		// Bangumi用户ID
-		userId: "1163581",
+		userId: "1203254",
 	},
 
 	// 页面开关配置 - 控制特定页面的访问权限，设为false会返回404
@@ -146,7 +146,7 @@ export const siteConfig: SiteConfig = {
 	// 统计分析
 	analytics: {
 		// Google Analytics ID
-		googleAnalyticsId: "G-P7GBNJKJKL",
+		googleAnalyticsId: "G-HXZW99KLLW",
 		// Microsoft Clarity ID
 		microsoftClarityId: "tx9equrgr6",
 	},
